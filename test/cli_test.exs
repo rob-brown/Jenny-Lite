3d0c3b30981cf7fec95be875c0ad85b570c45e21
Robert Brown <ammoknight@gmail.com>
2016-11-06 20:55:33 -0700

    Break out one test into many

--- test/cli_test.exs
+++ test/cli_test.exs
@@ -1,21 +1,45 @@
 defmodule JennyLite.CLI.Test do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   doctest JennyLite.CLI
 
   alias JennyLite.CLI
 
-  test "parse args" do
-    parse = (& &1 |> String.split(" ") |> Enum.reject(fn x -> x == "" end) |> CLI.parse_args)
+  test "parse empty args" do
     assert :no_files == CLI.parse_args []
-    assert parse.("--help") == :help
-    assert parse.("--help --file file") == :help
-    assert parse.("-h") == :help
-    assert parse.("help") == :help
-    assert parse.("--file file1") == %{file: ["file1"], ignore: []}
-    assert parse.("--file file1 --file file2") == %{file: ["file2", "file1"], ignore: []}
-    assert parse.("--file file1 --file file2 -- --file3") == %{file: ["file2", "file1", "--file3"], ignore: []}
-    assert parse.("-- file1") == %{file: ["file1"], ignore: []}
-    assert parse.("--ignore file1") == :no_files
-    assert parse.("--file file1 --ignore file2") == %{file: ["file1"], ignore: ["file2"]}
+  end
+
+  test "parse help switch" do
+    assert parse("--help") == :help
+    assert parse("--help --file file") == :help
+    assert parse("-h") == :help
+    assert parse("help") == :help
+  end
+
+  test "parse one file switch" do
+    assert parse("--file file1") == %{file: ["file1"], ignore: []}
+    assert parse("-- file1") == %{file: ["file1"], ignore: []}
+  end
+
+  test "parse many files switches" do
+    assert parse("--file file1 --file file2") == %{file: ["file2", "file1"], ignore: []}
+  end
+
+  test "parse file with -- prefix" do
+    assert parse("--file file1 --file file2 -- --file3") == %{file: ["file2", "file1", "--file3"], ignore: []}
+  end
+
+  test "parse no files" do
+    assert parse("--ignore file1") == :no_files
+  end
+
+  test "parse file and ignore" do
+    assert parse("--file file1 --ignore file2") == %{file: ["file1"], ignore: ["file2"]}
+  end
+
+  defp parse(arg_string) do
+    arg_string
+    |> String.split(" ")
+    |> Enum.reject(& &1 == "")
+    |> CLI.parse_args
   end
 end
